7ea83f52aaa00ed3f2c5bb7a507816ba69a865a0
Cameron Hunter <hello@cameronhunter.co.uk>
2023-11-19 21:06:08 -0800

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function hello(name: string) {
-    return `Hello ${name}!`;
-}
+import type { Config } from 'prettier';
+
+const config: Config = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'all',
+    useTabs: true,
+    overrides: [
+        {
+            files: ['*.json', '*.yaml', '*.yml'],
+            options: {
+                useTabs: false,
+                tabWidth: 2,
+            },
+        },
+    ],
+};
+
+exports = config;
--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import { hello } from '../src';
-
-describe('hello', () => {
-    test('it says hello', () => {
-        expect(hello('world')).toBe('Hello world!');
-    });
-});
