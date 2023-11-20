dd71f8b6e965b3b29a096f2f2722134dfdbdcf21
Cameron Hunter <hello@cameronhunter.co.uk>
2023-11-19 22:27:45 -0800

    Move back to tabs

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,19 @@
 import { test, expect } from 'vitest';
-import { format } from 'prettier';
+import { format, type Config } from 'prettier';
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
-import config from '../src/index';
+import config from '../.prettierrc.json';
 
-test('Formats a file as expected', async () => {
-    const beforePath = resolve(__dirname, '__fixtures__', 'before.ts');
+const suite = test.each`
+	fixture         | ext
+	${'typescript'} | ${'ts'}
+`;
 
-    const before = await readFile(beforePath, 'utf-8');
-    const after = await readFile(resolve(__dirname, '__fixtures__', 'after.ts'), 'utf-8');
+suite('$fixture', async ({ fixture, ext }) => {
+	const filepath = resolve(__dirname, '__fixtures__', fixture, `before.${ext}`);
 
-    await expect(format(before, { ...config, filepath: beforePath })).resolves.toEqual(after);
+	const before = await readFile(filepath, 'utf-8');
+	const after = await readFile(resolve(__dirname, '__fixtures__', fixture, `after.${ext}`), 'utf-8');
+
+	await expect(format(before, { ...(config as Config), filepath })).resolves.toEqual(after);
 });
